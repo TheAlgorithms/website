a147bc07b4d8e87d6ea35272becc8ebad1c988ea
maltejur <48161361+maltejur@users.noreply.github.com>
2020-12-02 15:38:00 +0100

    Improved search bar feedback

--- components/searchBar.jsx
+++ components/searchBar.jsx
@@ -5,39 +5,46 @@ import {
   InputLabel,
   InputAdornment,
   Icon,
-  Typography,
+  CircularProgress,
 } from "@material-ui/core";
 import { useRouter } from "next/router";
 
-const searchDebouncer = Debouncer(500);
+const searchDebouncer = Debouncer(200);
 
 export default function SearchBar() {
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
 
   return (
-    <form noValidate autoComplete="off">
+    <form noValidate autoComplete="off" onSubmit={handleSubmit}>
       <FormControl variant="filled" style={{ width: "100%" }}>
         <InputLabel>Search any algorithm</InputLabel>
         <FilledInput
-          onInput={
-            (event) =>
-              searchDebouncer(() => {
-                if (!event.target.value) router.push("/");
-                else router.push(`/search/${event.target.value}`);
-                // console.log(`/search/${event.target.value}`);
-              })
-            // dispatch({ key: "query", value: event.target.value });
-          }
+          onInput={(event) => {
+            setLoading(true);
+            searchDebouncer(() => {
+              if (!event.target.value)
+                router.push("/").then(() => setLoading(false));
+              else
+                router
+                  .push(`/search/${event.target.value}`)
+                  .then(() => setLoading(false));
+            });
+          }}
           name="q"
           endAdornment={
             <InputAdornment position="end">
-              <Icon>search</Icon>
+              {loading ? <CircularProgress size={24} /> : <Icon>search</Icon>}
             </InputAdornment>
           }
         />
       </FormControl>
     </form>
   );
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
 }
 
 function Debouncer(time) {
