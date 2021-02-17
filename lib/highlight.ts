6ff93af4a9c117e53bc3cd15cf476c1527ad8b4b
maltejur <48161361+maltejur@users.noreply.github.com>
2021-02-17 11:35:19 +0100

    icons and code highlighting for matlab and scala

--- lib/highlight.ts
+++ lib/highlight.ts
@@ -8,7 +8,8 @@ export default function highlightCode(code: string, language: string) {
       .replace("c-sharp", "cs")
       .replace("f-sharp", "fs")
       .replace("aarch64_assembly", "arm")
-      .replace("jupyter", "plaintext"),
+      .replace("jupyter", "plaintext")
+      .replace("matlab-octave", "matlab"),
     code,
     true
   ).value;
