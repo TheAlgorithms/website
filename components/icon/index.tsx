e70b9539246fadd38d2d193c49656b17d44167c9
Malte Jürgens <maltejur@web.de>
2021-02-23 19:23:44 +0100

    Add all missing icons

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -36,39 +36,39 @@ export default function LanguageIcon({
     case "c":
       return icon("devicon-c-plain");
     case "f-sharp":
-      return textIcon("F#");
+      return icon("devicon-fsharp-plain");
     case "go":
       return icon("devicon-go-plain");
     case "rust":
       return icon("devicon-rust-plain");
     case "aarch64_assembly":
-      return textIcon("A");
+      return icon("devicon-aarch64-plain");
     case "c-sharp":
       return icon("devicon-csharp-plain");
     case "dart":
       return icon("devicon-flutter-plain");
     case "r":
-      return textIcon("R");
+      return icon("devicon-r-plain");
     case "php":
       return icon("devicon-php-plain");
     case "elixir":
-      return textIcon("El");
+      return icon("devicon-elixir-plain");
     case "kotlin":
       return icon("devicon-kotlin-plain");
     case "scala":
       return icon("devicon-scala-plain");
     case "jupyter":
-      return textIcon("J");
+      return icon("devicon-jupyter-plain");
     case "haskell":
       return icon("devicon-haskell-plain");
     case "ocaml":
-      return textIcon("OC");
+      return icon("devicon-ocaml-plain");
     case "swift":
       return icon("devicon-swift-plain");
     case "elm":
       return icon("devicon-elm-plain");
     case "matlab-octave":
-      return textIcon("MLO");
+      return icon("devicon-matlab-plain");
     default:
       throw new Error(`Missing icon for ${language}`);
   }
