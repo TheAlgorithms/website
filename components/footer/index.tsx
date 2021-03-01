56932a2b5fa0e792d82f2dc09aedf6db6c509758
David Leal <halfpacho@gmail.com>
2021-03-01 16:31:48 -0600

    fix: Move the "Donate" link to the right side in the footer (#18)
    * fix: Move the "Donate" link to the right side...
    
    ...in the footer.
    
    * Switch "Donate" and "Source code" in the footer
    
    Co-authored-by: Malte Jürgens <maltejur@web.de>

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -31,15 +31,15 @@ export default function Footer() {
           <Link href="/#contribute">Programming Languages</Link>
           <Link href="/#contribute">Contribute</Link>
           <Link href="/#contribute">Donate</Link>
-          <Link href="https://github.com/TheAlgorithms/website">
-            Source code
-          </Link>
         </div>
         <div className={classes.list}>
           <Link href="https://github.com/TheAlgorithms/">GitHub</Link>
           <Link href="https://gitter.im/TheAlgorithms/">Gitter</Link>
           <Link href="https://twitter.com/The_Algorithms">Twitter</Link>
           <Link href="/all">All algorithms</Link>
+          <Link href="https://github.com/TheAlgorithms/website">
+            Source code
+          </Link>
         </div>
         <a
           className={classes.vercelLogo}
