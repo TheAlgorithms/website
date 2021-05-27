49f3195bbf97e4dc535fee5e0828827a53f00152
Malte Jürgens <maltejur@web.de>
2021-05-27 19:39:52 +0200

    test

--- components/link/index.tsx
+++ components/link/index.tsx
@@ -1,7 +1,7 @@
 import { Link as MuiLink } from "@material-ui/core";
 import NextLink from "next/link";
 
-export default function Link({ href, children, className = "", style = {} }) {
+export default function Link() {
   return (
     <NextLink href={href} passHref>
       <MuiLink className={className} style={style}>
