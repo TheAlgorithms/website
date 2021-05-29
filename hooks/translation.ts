62b59e37681d9d30b2f313c584e4f2f57d2c1d82
Malte Jürgens <maltejur@web.de>
2021-05-29 23:35:47 +0200

    small fix

--- hooks/translation.ts
+++ hooks/translation.ts
@@ -4,7 +4,7 @@ export default function useTranslation() {
   const { t } = useNextTranslation("common");
 
   return (name: string, variables: { [key: string]: string } = {}) => {
-    let str = t(name);
+    let str = t(name) || name;
     Object.keys(variables).forEach((from) => {
       str = str.replaceAll(`{${from}}`, variables[from]);
     });
