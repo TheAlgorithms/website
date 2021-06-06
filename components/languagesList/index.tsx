57b28a5c184d5562a7e9c8adf65b885c78aa5876
Malte Jürgens <maltejur@web.de>
2021-06-06 01:01:42 +0000

    Improve languages list (#87)
    * Improve languages list
    
    * small fix
    
    * fix
    
    * fix mobile view
    
    * sort after stars
    
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- components/languagesList/index.tsx
+++ components/languagesList/index.tsx
@@ -1,39 +1,80 @@
-import { ButtonBase, Card, CardContent, Typography } from "@material-ui/core";
-import NextLink from "next/link";
-import { Language, getLanguageName } from "lib/repositories";
+import {
+  List,
+  Paper,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  useMediaQuery,
+  IconButton,
+} from "@material-ui/core";
+import { Sort, Star } from "@material-ui/icons";
 import LanguageIcon from "components/icon";
+import { Language } from "lib/repositories";
+import Link from "next/link";
+import { useState } from "react";
 import classes from "./style.module.css";
+
+interface LanguageProp {
+  name: Language;
+  href: string;
+  stars?: number;
+}
 
 export default function LanguagesList({
   languages,
-  className,
-  outlined = false,
+  sortable = false,
 }: {
-  languages: {
-    name: Language;
-    href: string;
-  }[];
-  className?: string;
-  outlined?: boolean;
+  languages: LanguageProp[];
+  sortable?: boolean;
 }) {
+  const [reverse, setReverse] = useState(false);
+  const small = useMediaQuery("(max-width: 900px)");
+
   return (
-    <div className={className || ""}>
-      {languages.map((language) => (
-        <Card
-          variant={outlined ? "outlined" : "elevation"}
-          key={getLanguageName(language.name)}
-          className={classes.card}
+    <div className={classes.container}>
+      {sortable && (
+        <IconButton
+          className={reverse ? classes.sortReverse : classes.sort}
+          onClick={() => setReverse((r) => !r)}
         >
-          <NextLink href={language.href} passHref>
-            <ButtonBase>
-              <CardContent className={classes.cardInner}>
-                <LanguageIcon language={language.name} />
-                <Typography>{getLanguageName(language.name)}</Typography>
-              </CardContent>
-            </ButtonBase>
-          </NextLink>
-        </Card>
-      ))}
+          <Sort />
+        </IconButton>
+      )}
+      <List component={Paper} className={classes.list}>
+        {languages
+          .sort((a, b) => (reverse ? a.stars - b.stars : b.stars - a.stars))
+          .map((language) => (
+            <Link key={language.name} href={language.href} passHref>
+              <a className={classes.a}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <LanguageIcon language={language.name} />
+                  </ListItemIcon>
+                  <ListItemText
+                    secondary={
+                      small &&
+                      language.stars && (
+                        <>
+                          <Star fontSize="small" />
+                          {language.stars}
+                        </>
+                      )
+                    }
+                  >
+                    <span className={classes.disabled}>TheAlgorithms / </span>
+                    {language.name}
+                  </ListItemText>
+                  {!small && language.stars && (
+                    <ListItemText className={classes.stars}>
+                      <Star fontSize="small" />
+                      {language.stars}
+                    </ListItemText>
+                  )}
+                </ListItem>
+              </a>
+            </Link>
+          ))}
+      </List>
     </div>
   );
 }
