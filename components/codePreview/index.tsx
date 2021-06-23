0da2ae0a77f783806dcbdd3952967c38e9d63fdb
Malte Jürgens <maltejur@web.de>
2021-06-23 15:47:37 +0200

    add clickable backdrop to code preview

--- components/codePreview/index.tsx
+++ components/codePreview/index.tsx
@@ -4,7 +4,7 @@ import { Implementation } from "lib/models";
 import { getLanguageName, Language } from "lib/repositories";
 import useBodyScroll from "hooks/bodyScroll";
 import LanguageIcon from "components/icon";
-import { Button, Card, IconButton } from "@material-ui/core";
+import { Button, Card, IconButton, Paper } from "@material-ui/core";
 import { Close, OpenInNew } from "@material-ui/icons";
 import useTranslation from "hooks/translation";
 import classes from "./style.module.css";
@@ -23,10 +23,30 @@ export default function CodePreview({
   const t = useTranslation();
 
   useEffect(() => {
+    function backdropClickListener(event: MouseEvent) {
+      if ((event.target as HTMLDivElement).id === "__next") setActive(false);
+    }
+    document
+      .getElementById("__next")
+      .addEventListener("click", backdropClickListener);
+    return () => {
+      document
+        .getElementById("__next")
+        .removeEventListener("click", backdropClickListener);
+    };
+  }, []);
+
+  useEffect(() => {
     document.getElementsByTagName("html")[0].style.scrollBehavior = "smooth";
+    document.getElementById("__next").style.backgroundColor = active
+      ? "rgba(122,122,122,0.2)"
+      : "unset";
     setTimeout(() => {
-      if (active) window.scrollTo(0, 0);
-      else if (codeRef.current) codeRef.current.scrollTo(0, 0);
+      if (active) {
+        window.scrollTo(0, 0);
+      } else if (codeRef.current) {
+        codeRef.current.scrollTo(0, 0);
+      }
       setTimeout(
         () => {
           setBodyScroll(active);
@@ -36,6 +56,9 @@ export default function CodePreview({
         active ? 400 : 0
       );
     });
+    return () => {
+      document.getElementById("__next").style.backgroundColor = "unset";
+    };
   }, [active, setBodyScroll]);
 
   return (
@@ -57,64 +80,68 @@ export default function CodePreview({
         >
           {t("viewOnGithub")}
         </Button>
-        <IconButton color="primary" onClick={() => setActive(false)}>
-          <Close />
-        </IconButton>
+        <Paper className={classes.closeBtn}>
+          <IconButton color="primary" onClick={() => setActive(false)}>
+            <Close />
+          </IconButton>
+        </Paper>
       </div>
-      <div className={classes.scrollContainer}>
-        {selectedLanguague !== "jupyter" && (
-          <div
-            ref={codeRef}
-            className={classes.code}
-            onClick={() => setActive(true)}
-            role="button"
-            tabIndex={0}
-          >
-            <pre className={classes.pre}>
-              <code
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: implementations[selectedLanguague].code,
-                }}
-              />
-            </pre>
-          </div>
-        )}
-        <div className={classes.implementations}>
-          {Object.keys(implementations).map((language: Language) => (
-            <Card
-              key={language}
-              className={
-                language === selectedLanguague
-                  ? `${classes.card} ${classes.current}`
-                  : classes.card
-              }
+      <Paper className={classes.paper}>
+        <div className={classes.scrollContainer}>
+          {selectedLanguague !== "jupyter" && (
+            <div
+              ref={codeRef}
+              className={classes.code}
+              onClick={() => setActive(true)}
+              role="button"
+              tabIndex={0}
             >
-              <IconButton
-                className={classes.implementation}
-                onClick={() => {
-                  if (language !== "jupyter") {
-                    setActive(true);
-                    setSelectedLanguague(language);
-                  }
-                }}
-                href={
-                  language === "jupyter"
-                    ? implementations[language].url
-                    : undefined
+              <pre className={classes.pre}>
+                <code
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{
+                    __html: implementations[selectedLanguague].code,
+                  }}
+                />
+              </pre>
+            </div>
+          )}
+          <div className={classes.implementations}>
+            {Object.keys(implementations).map((language: Language) => (
+              <Card
+                key={language}
+                className={
+                  language === selectedLanguague
+                    ? `${classes.card} ${classes.current}`
+                    : classes.card
                 }
-                target="_blank"
-                rel="noreferrer"
-                aria-label={t("langImplementation", {
-                  language: getLanguageName(language),
-                })}
               >
-                <LanguageIcon language={language} />
-              </IconButton>
-            </Card>
-          ))}
+                <IconButton
+                  className={classes.implementation}
+                  onClick={() => {
+                    if (language !== "jupyter") {
+                      setActive(true);
+                      setSelectedLanguague(language);
+                    }
+                  }}
+                  href={
+                    language === "jupyter"
+                      ? implementations[language].url
+                      : undefined
+                  }
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label={t("langImplementation", {
+                    language: getLanguageName(language),
+                  })}
+                >
+                  <LanguageIcon language={language} />
+                </IconButton>
+              </Card>
+            ))}
+          </div>
         </div>
-      </div>
+      </Paper>
     </div>
   );
 }
