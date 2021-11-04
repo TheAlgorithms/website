b18c3063ef1ec29f61185d16671821cd4a970391
Malte Jürgens <maltejur@dismail.de>
2021-11-04 21:18:27 +0000

    feat(playground): load packages and add xterm (#132)

--- lib/pyodide.ts
+++ lib/pyodide.ts
@@ -3,9 +3,10 @@ import { loadPyodide } from "lib/pyodide/pyodide";
 let pyodide;
 
 export default async function tryLoadPyodide() {
-  if (!pyodide)
+  if (!pyodide) {
     pyodide = await loadPyodide({
       indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.0/full/",
     });
+  }
   return pyodide;
 }
