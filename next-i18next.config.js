258ca586a03be5c3687008e06fbba2291d707b3a
Malte Jürgens <maltejur@dismail.de>
2022-09-26 07:39:13 +0000

    fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,12 +1,16 @@
 /* eslint-disable */
 const path = require("path");
-require("dotenv").config()
+require("dotenv").config();
 
 const locales = require("./lib/locales");
 module.exports = {
   i18n: {
     defaultLocale: "en",
-    locales: (!process.env.VERCEL || (process.env.S3_ACCESS_KEY_ID && process.env.S3_SECRET_ACCESS_KEY)) ? locales.map((locale) => locale.code) : "en",
+    locales:
+      !process.env.VERCEL ||
+      (process.env.S3_ACCESS_KEY_ID && process.env.S3_SECRET_ACCESS_KEY)
+        ? locales.map((locale) => locale.code)
+        : ["en"],
     localePath: path.resolve("./public/locales"),
   },
 };
