8c9a25f44d14b9d8999fbd2a6d2b3cbcd23fea25
Anurag Sharma <anuragsharma011011@gmail.com>
2022-09-27 21:20:22 +0530

    Use correct Discord invitation link (#186)
    * added hindi language
    
    * added correct discord invite link

--- pages/[shortlink].tsx
+++ pages/[shortlink].tsx
@@ -7,7 +7,7 @@ export default function Shortlink() {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const URLs = {
-    discord: "https://the-algorithms.com/discord/",
+    discord: "https://discord.gg/c7MnfGFGa6",
   };
   if (context.params.shortlink && URLs[context.params.shortlink.toString()])
     return {
