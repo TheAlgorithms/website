dfb38f719cd148bf77889334c8e272b47869c251
Malte Jürgens <maltejur@dismail.de>
2023-03-09 18:47:17 +0100

    highlight zig code as rust

--- lib/highlight.ts
+++ lib/highlight.ts
@@ -9,7 +9,8 @@ export default function highlightCode(code: string, language: string) {
       .replace("f-sharp", "fs")
       .replace("aarch64_assembly", "arm")
       .replace("jupyter", "json")
-      .replace("matlab-octave", "matlab"),
+      .replace("matlab-octave", "matlab")
+      .replace("zig", "rust"),
   }).value;
   return result;
 }
